5b96316b8db0a5351009382e0bc63a5496ef5100
pjqpdqjdp <sjakaj123@gmail.com>
2025-01-14 14:34:01 +0300

    first commit

---
+++ obj/Release/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		mono.android.Runtime.register ("AppPoki.MainApplication, AppPoki, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6426959e5b13e3649b.MainApplication.class, crc6426959e5b13e3649b.MainApplication.__md_methods);
+		
+	}
+}
